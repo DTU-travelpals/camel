6895130ec24a94452af310c81c32ba9695a895c2
DTU-travelpals <daniel.tomsu@travelpals.de>
2016-06-16 23:55:01 +0200

    added  message3 for master

--- src/main/java/de/tomsu/camel/MyRouteBuilder.java
+++ src/main/java/de/tomsu/camel/MyRouteBuilder.java
@@ -15,12 +15,15 @@ public class MyRouteBuilder extends RouteBuilder {
         // here is a sample which processes the input files
         // (leaving them in place - see the 'noop' flag)
         // then performs content based routing on the message using XPath
-    	// removed: ?noop=true
+        // removed: ?noop=true
         from("file:src/data")
-            .choice()
-                .when(xpath("/person/city = 'London'"))
+                .choice()
+                    .when(xpath("/person/city = 'London'"))
                     .log("UK message")
                     .to("file:target/messages/uk")
+                .when(xpath("/person/city = 'Friedrichshafen'"))
+                    .log("DE message")
+                    .to("file:target/messages/de")
                 .otherwise()
                     .log("Other message")
                     .to("file:target/messages/others");
