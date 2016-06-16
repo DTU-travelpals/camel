f361e7262919432d2c66576939148ad14de750ca
Daniel Tomsu <daniel.tomsu@travelpals.de>
2016-06-16 21:53:35 +0200

    removed the noop flag

---
+++ src/main/java/de/tomsu/camel/MyRouteBuilder.java
@@ -0,0 +1,29 @@
+package de.tomsu.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+
+/**
+ * A Camel Java DSL Router
+ */
+public class MyRouteBuilder extends RouteBuilder {
+
+    /**
+     * Let's configure the Camel routing rules using Java code...
+     */
+    public void configure() {
+
+        // here is a sample which processes the input files
+        // (leaving them in place - see the 'noop' flag)
+        // then performs content based routing on the message using XPath
+    	// removed: ?noop=true
+        from("file:src/data")
+            .choice()
+                .when(xpath("/person/city = 'London'"))
+                    .log("UK message")
+                    .to("file:target/messages/uk")
+                .otherwise()
+                    .log("Other message")
+                    .to("file:target/messages/others");
+    }
+
+}
